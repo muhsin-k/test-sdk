1669ffcb2b8a370ba09a65f20ee13c90b2b2ee3f
Vishnu Narayanan <iamwishnu@gmail.com>
2025-05-21 12:39:22 +0530

    debug: fix jitpack publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -71,7 +71,7 @@ afterEvaluate {
                 
                 groupId = "com.github.muhsin-k"
                 artifactId = "chatwoot-sdk"
-                version = "1.0.2"
+                version = "1.0.3"
             }
         }
     }
