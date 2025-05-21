3eace6fdd2cc8a3b2a7aedaa2f2eab3ab61ddedd
Vishnu Narayanan <iamwishnu@gmail.com>
2025-05-21 13:16:36 +0530

    chore: switch to gradle 6.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -82,7 +82,7 @@ afterEvaluate {
                 
                 groupId = "com.github.muhsin-k"
                 artifactId = "chatwoot-sdk"
-                version = "1.0.6"
+                version = "1.0.9"
             }
         }
     }
