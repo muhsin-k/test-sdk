f286352bf8b551439d1657d73390547dc0424521
Vishnu Narayanan <iamwishnu@gmail.com>
2025-05-21 12:31:33 +0530

    fix: jitpack library  publishing

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-## Test SDK
+# ChatwootSDK
+
+Android SDK for Chatwoot
+
+## Development Setup
+
+Before building or publishing, please run:
+
+```bash
+./gradlew wrapper --gradle-version 8.0
+```
+
+This will generate the required Gradle wrapper JAR file in the gradle/wrapper directory.
+
+## Usage in other projects
+
+Add the JitPack repository to your build file:
+
+```kotlin
+allprojects {
+    repositories {
+        ...
+        maven(url = "https://jitpack.io")
+    }
+}
+```
+
+Add the dependency:
+
+```kotlin
+dependencies {
+    implementation("com.github.muhsin-k:chatwoot-sdk:v1.0.1")
+}
+```
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-parcelize")
+    id("maven-publish")
 }
 
 android {
@@ -9,6 +11,7 @@ android {
 
     defaultConfig {
         minSdk = 21
+        targetSdk = 34
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
@@ -22,20 +25,60 @@ android {
             )
         }
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
     }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.webkit:webkit:1.8.0")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("androidx.webkit:webkit:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("io.coil-kt:coil:2.5.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-} 
+}
+
+// Configure maven publication
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                
+                groupId = "com.github.muhsin-k"
+                artifactId = "chatwoot-sdk"
+                version = "1.0.1"
+            }
+        }
+    }
+}
+
+// Make sure JitPack can find the proper repositories
+repositories {
+    google()
+    mavenCentral()
+}
